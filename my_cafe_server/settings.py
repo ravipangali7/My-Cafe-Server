f3a00498cadff8bf49c78c12910424cb7393a726
Marmajan Rokaya <marmajanrokaya6@gmail.com>
2026-01-27 15:50:48 +0545

    CHanges

--- my_cafe_server/settings.py
+++ my_cafe_server/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
